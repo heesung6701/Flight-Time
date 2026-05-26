2830c5de70e1a8bbfb02385806b8ca9880881b57
hee.youn <hee.youn@samsung.com>
2026-05-26 19:26:11 +0900

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import App from "./App.jsx";
 import "../styles.css";
 
@@ -13,6 +14,7 @@ function Root() {
     <>
       <App />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
