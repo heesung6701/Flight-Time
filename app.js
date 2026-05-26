9c860a15a566b515ddcfa06b4f9f304c4039beb2
hee.youn <hee.youn@samsung.com>
2026-05-26 16:26:04 +0900

    docs: add aircraft type batch PRD

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import {
   parseOriginalRows,
   parseTsv,
   serializeAircraftTypeMap,
-} from "./src/core/flighttime-core.js?v=0.1.17";
+} from "./src/core/flighttime-core.js?v=0.1.18";
 
 const CONFIG_STORAGE_KEY = "flightTimeAircraftTypes";
 const AIRPORT_CACHE_KEY = "flightTimeAirportsByIata";
